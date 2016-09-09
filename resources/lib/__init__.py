050b9ecb2cf833e16a73a99db7acc86612b1e5ab
Harry G <harrygg@gmail.com>
2016-09-09 14:57:04 +0300

    1st working version

---
+++ resources/lib/__init__.py
@@ -0,0 +1,92 @@
+# -*- coding: utf8 -*-
+import os, sys, requests, re
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+class Playlist:
+  name = 'playlist.m3u'
+  channels = []
+  raw_m3u = None
+  append = True
+  
+  def __init__(self, log, name = ''):
+    self.log = log
+    if name != '':
+      self.name = name
+  
+  def save(self, path):
+    file_path = os.path.join(path, self.name)
+    self.log("Запазване на плейлистата: %s " % file_path)
+    if os.path.exists(path):
+      with open(file_path, 'w') as f:
+        f.write(self.to_string().encode('utf-8', 'replace'))
+  
+  def concat(self, new_m3u, append = True, raw = True):
+    if raw: #TODO implement parsing playlists
+      self.append = append
+      with open(new_m3u, 'r') as f:
+        self.raw_m3u = f.read().replace('#EXTM3U', '')
+  
+  def to_string(self):
+    output = ''
+    for c in self.channels:
+      output += c.to_string()
+      
+    if self.raw_m3u != None:
+      if self.append:
+        output += self.raw_m3u
+      else:
+        output = self.raw_m3u + output
+    
+    return '#EXTM3U\n' + output
+  
+class Channel:
+  #user_agent = 'stagefright/2.0'
+  user_agent = 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
+  
+  def __init__(self, attr):
+    self.id = attr[0]
+    self.disabled = attr[1] == 1
+    self.name = attr[2]
+    self.category = attr[3]
+    self.logo = attr[4]
+    self.streams = attr[5]
+    self.playpath = '' if attr[6] == None else attr[6]
+    self.epg_id = '' if attr[9] == None else attr[9]
+    self.user_agent = False if attr[10] == None else attr[10]
+
+  def to_string(self):
+    output = '#EXTINF:-1 radio="False" tvg-shift=0 group-title="%s" tvg-logo="%s" tvg-id="%s",%s\n' % (self.category, self.logo, self.epg_id, self.name)
+    output += '%s' % self.playpath
+    if self.user_agent: 
+      output += '|User-Agent=%s' % self.user_agent
+    return output + '\n'
+ 
+class Stream:
+  def __init__(self, attr, log):
+    self.log = log
+    self.id = attr[0] 
+    self.channel_id = attr[1] 
+    self.url = attr[2]
+    self.page_url = attr[3]
+    self.player_url = attr[4]
+    self.disabled = attr[5] == 1
+    self.comment = attr[6]
+    self.user_agent = 'Mozilla/5.0 (Linux; Android 6.0; LG-H960; Build/WRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
+    if self.url == None:
+      self.url = self.resolve()
+    
+  def resolve(self):
+    #if '3583019' in self.player_url: #BiT
+    #	return self._livestream()
+    headers = {'User-agent': self.user_agent, 'Referer':self.page_url}
+    self.log("Resolving stream from: %s" % self.player_url)
+    res = requests.get(self.player_url, headers=headers)
+    m = re.compile('(http.*m3u.*?)[\s\'"]+').findall(res.text)
+    self.log('Found %s m3u matches in source ' % len(m))
+    stream = '' if len(m) == 0 else m[0]
+    #travelhd wrong stream name hack
+    if 'playerCommunity' in self.player_url:
+      stream.replace('/community/community', '/travel/community')
+    return stream
--- resources/lib/assets.py
+++ resources/lib/assets.py
@@ -1,61 +1,75 @@
 # -*- coding: utf8 -*-
-import os, gzip, urllib2, xbmc
+import os, sys, gzip, urllib2
+
+### Basic class to download assets on a give time interval
+### If download fails, and the old file doesn't exist
+### Use a local file
 
 class Assets:
-  db_name = 'tv.db'
+  interval = 24 #Hours Interval to check for new version of the asset
   
-  def __init__(self, profile_path, remote_db):
-    self.db_path = os.path.join(profile_path, self.db_name)
-    self.remote_db = remote_db
-    if self.is_update_assets():
-      self.get_assets()
-
-  def is_update_assets(self):
+  def __init__(self, temp_dir, url, backup_file, log):
+    if os.path.isdir(temp_dir) is False:
+      create_dir(temp_dir)
+    
+    if url == '':
+      raise ValueError("Valid asset url must be provided!")
+    else:
+      self.log = log
+      self.url = url
+      self.file_name = os.path.basename(url)
+      self.file = os.path.join(temp_dir, self.file_name)
+      self.backup_file = backup_file
+      if os.path.isfile(self.file):
+        self.first_run = False
+    if self.is_expired():
+      self.get_asset()
+    if self.file.endswith('gz'):
+      self.extract()
+      
+  def is_expired(self):
     try:
       from datetime import datetime, timedelta
-      if os.path.exists(self.db_path):
-        treshold = datetime.now() - timedelta(hours=24)
-        fileModified = datetime.fromtimestamp(os.path.getmtime(self.db_path))
-        if fileModified < treshold: #file is more than a day old
+      if os.path.isfile(self.file):
+        treshold = datetime.now() - timedelta(hours=self.interval)
+        modified = datetime.fromtimestamp(os.path.getmtime(self.file))
+        if modified < treshold: #file is more than a day old
           return True
         return False
       else: #file does not exist, perhaps first run
         return True
     except Exception, er:
-      xbmc.log(str(er))
-      return False
+      self.log(str(er), 4)
+      return True
 
-  def get_assets(self):
+  def get_asset(self):
     try:
-      xbmc.log('Downloading assets from url: %s' % self.remote_db)
-      save_to_file = self.db_path if '.gz' not in self.remote_db else self.db_path + ".gz"
-      f = urllib2.urlopen(self.remote_db)
-      if not os.path.exists(os.path.dirname(save_to_file)):
-        create_dir(save_to_file)
-      with open(save_to_file, "wb") as code:
+      self.log('Downloading assets from url: %s' % self.url)
+      f = urllib2.urlopen(self.url)
+      with open(self.file, "wb") as code:
         code.write(f.read())
-      self.extract(save_to_file)
-    except Exception, er:
-      xbmc.log(str(er))
-      xbmc.executebuiltin('Notification(%s,%s,10000,%s)' % ('Assets', 'Неуспешно сваляне на най-новата база данни!',''))
-      assets = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../%s.gz' % self.db_name)
-      self.extract(assets)
-      
-  def create_dir(path):
-    try: os.makedirs(os.path.dirname(path))
+    except:
+      self.handle_ex()
+
+  def create_dir(dir):
+    try: os.makedirs(dir)
     except OSError as exc: # Guard against race condition
       if exc.errno != errno.EEXIST:
         raise
       
-  def extract(self, path):
+  def extract(self):
     try:
-      gz = gzip.GzipFile(path, 'rb')
-      s = gz.read()
-      gz.close()
-      if not os.path.exists(os.path.dirname(self.db_path)):
-        create_dir(self.db_path)      
-      out = file(self.db_path, 'wb')
-      out.write(s)
-      out.close()
+      with gzip.GzipFile(self.file, 'rb') as gz:
+        gz.read()
+      self.file = self.file.replace('.gz', '')
+      with file(self.file, 'wb') as out:
+        out.write(s)
     except:
-      raise
+      self.handle_ex()
+      
+  def handle_ex(self):
+    import traceback
+    type, errstr = sys.exc_info()[:2]
+    self.log('Unable to download assets file!\n' + str(sys.exc_info()[0]) + ': ' + str(sys.exc_info()[1]) + ''.join(traceback.format_stack()), 4)
+    if not os.path.isfile(self.file) and os.path.isfile(self.backup_file): #if asset was never downloaded and backup exists
+      self.file = self.backup_file
